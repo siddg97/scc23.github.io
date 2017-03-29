66a841f2544887282c1b2d2650766e90d13bf63a
Sherman Chow <SChow@sierrawireless.com>
2017-03-30 00:50:49 +0100

    add food in canvas

--- sketch.js
+++ sketch.js
@@ -1,16 +1,23 @@
+// global variables
 var s;
 var scl = 20;
+
+var food;
 
 function setup() {
 	createCanvas(600, 600);
 	s = new Snake();
 	frameRate(10);
+	food = createVector(random(width), random(height));
 }
 
 function draw() {
 	background(51);
 	s.update();
 	s.show();
+
+	fill(255, 0, 100);
+	rect(food.x, food.y, scl, scl);
 }
 
 function keyPressed() {
