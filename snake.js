1b47f34e3845b8c8dd7a68c1ea58fe52c30d8745
Sherman Chow <SChow@sierrawireless.com>
2017-03-30 00:38:29 +0100

    add function in constructor

--- snake.js
+++ snake.js
@@ -4,6 +4,11 @@ function Snake() {
 	this.xspeed = 1;
 	this.yspeed = 0;
 
+	this.dir = function(x, y) {
+		this.xspeed = x;
+		this.yspeed = y;
+	}
+
 	this.update = function() {
 		this.x = this.x + this.xspeed;
 		this.y = this.y + this.yspeed;
