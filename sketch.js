81f54117083d69cfcad16f09fb90174d2733dea8
Sherman Chow <SChow@sierrawireless.com>
2017-03-30 00:31:12 +0100

    added js code

--- sketch.js
+++ sketch.js
@@ -11,19 +11,17 @@ function draw() {
 	s.show();
 }
 
-function Snake() {
-	this.x = 0;
-	this.y = 0;
-	this.xspeed = 1;
-	this.yspeed = 0;
-
-	this.update = function() {
-		this.x = this.x + this.xspeed;
-		this.y = this.y + this.yspeed;
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		s.dir(0, -1);
 	}
-
-	this.show = function() {
-		fill(255);
-		rect(this.x, this.y, 10, 10);
+	else if (keyCode === DOWN_ARROW) {
+		s.dir(0, 1);
+	}
+	else if (keyCode === RIGHT_ARROW) {
+		s.dir(1, 1);
+	}
+	else if (keyCode === LEFT_ARROW) {
+		s.dir(-1, 1);
 	}
 }
