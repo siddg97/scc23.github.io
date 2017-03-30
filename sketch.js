bf432731089a5e76ad682e1b5c23c670b3e16156
Sherman Chow <scc23@sfu.ca>
2017-03-30 00:22:59 -0700

    bug fixes

--- sketch.js
+++ sketch.js
@@ -44,15 +44,15 @@ function keyPressed() {
 		s.dir(0, -1);
 	}
 	// down arrow or S
-	else if (keyCode === DOWN_ARROW || keyCode == 83) {
+	else if (keyCode === DOWN_ARROW) {
 		s.dir(0, 1);
 	}
 	// right arrow or D
-	else if (keyCode === RIGHT_ARROW || keyCode == 68) {
+	else if (keyCode === RIGHT_ARROW) {
 		s.dir(1, 0);
 	}
 	// left arrow or A
-	else if (keyCode === LEFT_ARROW || keyCode == 65) {
+	else if (keyCode === LEFT_ARROW) {
 		s.dir(-1, 0);
 	}
 }
