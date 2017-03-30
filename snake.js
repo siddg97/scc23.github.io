e6b93a77e553b82e99b8cdda334de069f42ad30f
Sherman Chow <scc23@sfu.ca>
2017-03-29 18:24:32 -0700

    testing

--- snake.js
+++ snake.js
@@ -1,64 +1,124 @@
-// constructor function for snake
+// // constructor function for snake
+// function Snake() {
+// 	// initialize objects
+// 	this.x = 0;
+// 	this.y = 0;
+// 	this.xspeed = 1;
+// 	this.yspeed = 0;
+// 	this.total = 0;
+// 	this.tail = [];		// empty array for tail to add on
+
+// 	this.dir = function(x, y) {
+// 		this.xspeed = x;
+// 		this.yspeed = y;
+// 	}
+
+// 	// function to determine if snake ate food
+// 	this.eat = function(pos) {
+// 		var d = dist(this.x, this.y, pos.x, pos.y);
+// 		// whether or not snake reached the food
+// 		if (d < 1) {
+// 			this.total++;	// if snake ate food, increment total
+// 			return true;
+// 		}
+// 		else {
+// 			return false;
+// 		}
+// 	}
+
+// 	// function to determine if snake died
+// 	this.death = function() {
+// 		for (var i = 0; i < this.tail.length; i++) {
+// 			var pos = this.tail[i];
+// 			var d = dis(this.x, this.y, pos.x, pos.y);
+// 			// if died, reset everything
+// 			if (d < 1) {
+// 				total = 0;
+// 				this.tail = [];
+// 			}
+// 		}
+// 	}
+
+// 	this.update = function() {
+// 		if (this.total === this.tail.length) {
+// 			for (var i = 0; i < this.tail.length-1; i++) {
+// 				this.tail[i] = this.tail[i+1];
+// 			}
+// 		}
+// 		this.tail[this.total-1] = createVector(this.x, this.y);
+
+// 		this.x = this.x + this.xspeed * scl;
+// 		this.y = this.y + this.yspeed * scl;
+
+// 		this.x = constrain(this.x, 0, width-scl);
+// 		this.y = constrain(this.y, 0, height-scl);
+// 	}
+
+// 	this.show = function() {
+// 		fill(255);
+// 		for (var i = 0; i < this.tail.length; i++) {
+// 			rect(this.tail[i].x, this.tail[i].y, scl, scl);
+// 		}
+// 		rect(this.x, this.y, scl, scl);
+// 	}
+// }
+
 function Snake() {
-	// initialize objects
-	this.x = 0;
-	this.y = 0;
-	this.xspeed = 1;
-	this.yspeed = 0;
-	this.total = 0;
-	this.tail = [];		// empty array for tail to add on
+  this.x = 0;
+  this.y = 0;
+  this.xspeed = 1;
+  this.yspeed = 0;
+  this.total = 0;
+  this.tail = [];
 
-	this.dir = function(x, y) {
-		this.xspeed = x;
-		this.yspeed = y;
-	}
+  this.eat = function(pos) {
+    var d = dist(this.x, this.y, pos.x, pos.y);
+    if (d < 1) {
+      this.total++;
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-	// function to determine if snake ate food
-	this.eat = function(pos) {
-		var d = dist(this.x, this.y, pos.x, pos.y);
-		// whether or not snake reached the food
-		if (d < 1) {
-			this.total++;	// if snake ate food, increment total
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+  this.dir = function(x, y) {
+    this.xspeed = x;
+    this.yspeed = y;
+  }
 
-	// function to determine if snake died
-	this.death = function() {
-		for (var i = 0; i < this.tail.length; i++) {
-			var pos = this.tail[i];
-			var d = dis(this.x, this.y, pos.x, pos.y);
-			// if died, reset everything
-			if (d < 1) {
-				total = 0;
-				this.tail = [];
-			}
-		}
-	}
+  this.death = function() {
+    for (var i = 0; i < this.tail.length; i++) {
+      var pos = this.tail[i];
+      var d = dist(this.x, this.y, pos.x, pos.y);
+      if (d < 1) {
+        console.log('starting over');
+        this.total = 0;
+        this.tail = [];
+      }
+    }
+  }
 
-	this.update = function() {
-		if (this.total === this.tail.length) {
-			for (var i = 0; i < this.tail.length-1; i++) {
-				this.tail[i] = this.tail[i+1];
-			}
-		}
-		this.tail[this.total-1] = createVector(this.x, this.y);
+  this.update = function() {
+    if (this.total === this.tail.length) {
+      for (var i = 0; i < this.tail.length - 1; i++) {
+        this.tail[i] = this.tail[i + 1];
+      }
+    }
+    this.tail[this.total - 1] = createVector(this.x, this.y);
 
-		this.x = this.x + this.xspeed * scl;
-		this.y = this.y + this.yspeed * scl;
+    this.x = this.x + this.xspeed * scl;
+    this.y = this.y + this.yspeed * scl;
 
-		this.x = constrain(this.x, 0, width-scl);
-		this.y = constrain(this.y, 0, height-scl);
-	}
+    this.x = constrain(this.x, 0, width - scl);
+    this.y = constrain(this.y, 0, height - scl);
+  }
 
-	this.show = function() {
-		fill(255);
-		for (var i = 0; i < this.tail.length; i++) {
-			rect(this.tail[i].x, this.tail[i].y, scl, scl);
-		}
-		rect(this.x, this.y, scl, scl);
-	}
+  this.show = function() {
+    fill(255);
+    for (var i = 0; i < this.tail.length; i++) {
+      rect(this.tail[i].x, this.tail[i].y, scl, scl);
+    }
+    rect(this.x, this.y, scl, scl);
+
+  }
 }
