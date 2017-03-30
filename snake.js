ea008fbf673ec695391fe1f376727ab17837519b
Sherman Chow <SChow@sierrawireless.com>
2017-03-30 18:21:58 +0100

    fix bugs

--- snake.js
+++ snake.js
@@ -28,13 +28,13 @@ function Snake() {
 
 	// function to determine if snake died (player lost)
 	this.gameOver = function() {
-		// if snake hit edge of box
-		if (this.x > canvasSize || thix.y > canvasSize) {
-			this.x = 300;
-			this.y = 300;
-			this.total = 0;
-			this.tail = [];
-		}
+		// // if snake hit edge of box
+		// if (this.x > canvasSize || thix.y > canvasSize) {
+		// 	this.x = 300;
+		// 	this.y = 300;
+		// 	this.total = 0;
+		// 	this.tail = [];
+		// }
 
 		for (var i = 0; i < this.tail.length; i++) {
 			var pos = this.tail[i];
