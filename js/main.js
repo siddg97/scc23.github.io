43089d57338e381e266b8496ba6a612acd2f6546
Sherman Chow <scc23@sfu.ca>
2017-04-07 23:44:55 -0700

    added updated files, mobile friendly

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// alert("SUP FAM");
+// document.body.style.backgroundColor = "orange";
+
+$(document).ready(function() {
+
+	$('#navigation').hide();
+
+	$('#menu-button').click(function() {
+		$('#navigation').toggle();
+	});
+
+});
